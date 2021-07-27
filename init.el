76e06fbcff2ef9344c6e3bd396245f8f29cd5172
qianyi liu <liuqianyi125@gmail.com>
2021-07-27 15:35:39 +0800

    update init.el
    1. add org-download annotation hidden
    2. add org-properties-toggle func

--- init.el
+++ init.el
@@ -549,7 +549,33 @@
           org-roam-server-network-label-truncate-length 60
           org-roam-server-network-label-wrap-length 20)
 
+    ;; hidden org-download annotation
     (setq org-download-annotate-function (lambda (_link) ""))
+
+    (defun org-hide-properties ()
+      "Hide all org-mode headline property drawers in buffer. Could be slow if it has a lot of overlays."
+      (interactive)
+      (save-excursion
+        (goto-char (point-min))
+        (while (re-search-forward
+                "^ *:properties:\n\\( *:.+?:.*\n\\)+ *:end:\n" nil t)
+          (let ((ov_this (make-overlay (match-beginning 0) (match-end 0))))
+            (overlay-put ov_this 'display "")
+            (overlay-put ov_this 'hidden-prop-drawer t))))
+      (put 'org-toggle-properties-hide-state 'state 'hidden))
+
+    (defun org-show-properties ()
+      "Show all org-mode property drawers hidden by org-hide-properties."
+      (interactive)
+      (remove-overlays (point-min) (point-max) 'hidden-prop-drawer t)
+      (put 'org-toggle-properties-hide-state 'state 'shown))
+
+    (defun org-toggle-properties ()
+      "Toggle visibility of property drawers."
+      (interactive)
+      (if (eq (get 'org-toggle-properties-hide-state 'state) 'hidden)
+          (org-show-properties)
+        (org-hide-properties)))
   )
 
 ;; Do not write anything past this comment. This is where Emacs will
