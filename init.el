ee13de760c7203c6572e09ef686a86bf9477ed61
qianyi liu <liuqianyi125@gmail.com>
2021-08-04 20:27:32 +0800

    add graphviz layer

--- init.el
+++ init.el
@@ -114,6 +114,7 @@
      shell-scripts
      (lsp :variables lsp-lens-enable t)
      semantic
+     graphviz
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
