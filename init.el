e3ed90d86862963afa046d1e31f49ab580f7ae80
qianyi liu <liuqianyi125@gmail.com>
2023-04-19 15:37:14 +0800

    font size default to 26.
    Signed-off-by: qianyi liu <liuqianyi125@gmail.com>

--- init.el
+++ init.el
@@ -189,7 +189,7 @@
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
    dotspacemacs-default-font '("YaHei Consolas Hybrid"
-                               :size 18
+                               :size 26
                                :weight normal
                                :width normal
                                :powerline-scale 1.1)
