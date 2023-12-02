fe43ef4c4f7d5131a5c022e830729722df3dbfd9
qianyi.liu <liuqianyi125@gmail.com>
2023-12-02 21:02:22 +0800

    auto close compilation window when it success
    Signed-off-by: qianyi.liu <liuqianyi125@gmail.com>

--- init.el
+++ init.el
@@ -751,6 +751,23 @@
             ("\\.[Cc]+[Pp]*\\'" . "g++ -g -Wall %f  -o %n")
             )
           )
+
+    (defun bury-compile-buffer-if-successful (buffer string)
+      "Bury a compilation buffer if succeeded without warnings "
+      (when (and
+             (buffer-live-p buffer)
+             (string-match "compilation" (buffer-name buffer))
+             (string-match "finished" string)
+             (not
+              (with-current-buffer buffer
+                (goto-char (point-min))
+                (search-forward "warning" nil t))))
+        (run-with-timer 1 nil
+                        'delete-windows-on
+                        (get-buffer-create "*compilation*")
+                        )
+        (message "No Compilation Warnings or Errors!")))
+    (add-hook 'compilation-finish-functions 'bury-compile-buffer-if-successful)
   )
 
 ;; Do not write anything past this comment. This is where Emacs will
