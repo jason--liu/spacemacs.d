a2644df8a6d3e888e0badf869fc1ae7f233b3328
qianyi.liu <liuqianyi125@gmail.com>
2024-05-03 12:34:54 +0800

    change default smart-compile alist
    Signed-off-by: qianyi.liu <liuqianyi125@gmail.com>

--- init.el
+++ init.el
@@ -751,7 +751,7 @@
     ;; http://blog.chinaunix.net/uid-20982635-id-1831343.html
     (setq smart-compile-alist
           '(("\\.c\\'" . "gcc -g -Wall %f -o %n.bin")
-            ("\\.[Cc]+[Pp]*\\'" . "g++ -g -Wall %f  -o %n")
+            ("\\.[Cc]+[Pp]*\\'" . "g++ -g -Wall %f  -o %n.bin")
             )
           )
 
